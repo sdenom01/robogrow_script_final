780fce7128900bf54db2b398fc3112471e555e57
Shane Denomme <sdenomme15@gmail.com>
2021-01-20 02:32:55 -0500

    Potentially fixed relay issue

--- script.js
+++ script.js
@@ -440,7 +440,7 @@ function ScheduleRelays() {
                     var curDate = moment(event.triggerTime, 'HH:mm:ss');
                     var nextDate = moment(nextEvent.triggerTime, 'HH:mm:ss');
 
-                    if (!isToday) {
+                    if (!isToday && schedule.events.length > 0) {
                         // Event takes place tomorrrow add 24 hours to nextEvent (for 'current event')
                         curDate = curDate.subtract(24, 'hours');
                     }
